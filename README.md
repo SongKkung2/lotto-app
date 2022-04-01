64a4f245066d0fbb342406221ffc35590b203266
SongKkung2 <grave_no@naver.com>
2022-04-01 13:00:27 +0900

    껑껑이 로또 완성

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # lotto-app
 node.js 로 구현하는 로또 웹 앱
+## 작성자 : 송채은
+## 작성기간 : 3월 27일 ~ 4월 1일
+## 세부내용
+1. 컨셉 지정.
+2. ux 스케치
+3. html5 작성
+4. css 작성
+5. node,js 세팅 
+6. node 패키지 : express, nodemon, ngrok 
+7. 안정화작업
--- public/script/routing.js
+++ public/script/routing.js
@@ -1,4 +1,4 @@
-// 자바스크립트 변수는 var     vs let, const
+
 // 차이점              구식         신식
 // 문제점! var 는 선언하지 않고도 사용 가능함, 전역/지역 명확하지 않음.
 
@@ -18,4 +18,27 @@ function sideOpen(){
 }
 function sideClose(){
     sidePan.classList.add("d-none");
+}
+
+const toggleBTN = document.querySelector("#blind")
+
+toggleBTN.addEventListener("click", handleClick)
+
+let btnStatus = "lock"
+
+function handleClick(e){
+    if(e.target.className == btnStatus) {
+        unlock("해제");                        
+    } else {
+        lock("잠금");
+    }
+}
+
+function unlock(){
+    toggleBTN.className="unlock"
+    toggleBTN.textContent = "해제"
+}
+function lock(){
+    toggleBTN.className="lock"
+    toggleBTN.textContent = "잠금"
 }
